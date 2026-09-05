21433ead04fb8a9e0d47d7b5dfd91745301fb8a6
jordybeer <info@jordy.beer>
2026-09-05 14:33:57 +0200

    fix: avoid late duplicate Telegram bridge load

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,11 +1,5 @@
-import Script from "next/script";
 import MapClient from "./MapClient";
 
 export default function MapPage() {
-  return (
-    <>
-      <Script src="https://telegram.org/js/telegram-web-app.js?63" strategy="beforeInteractive" />
-      <MapClient />
-    </>
-  );
+  return <MapClient />;
 }
