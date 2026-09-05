d0fe1602e2d8815281aa21a8c603d1e0ff84219d
jordybeer <info@jordy.beer>
2026-09-05 23:39:48 +0200

    fix: serve clear festival map from embedded source

---
+++ src/data/map-image/index.ts
@@ -0,0 +1,11 @@
+import chunk0 from "./chunk0";
+import chunk1 from "./chunk1";
+import chunk2 from "./chunk2";
+import chunk3a from "./chunk3a";
+import chunk3b from "./chunk3b";
+import chunk4 from "./chunk4";
+
+export const FESTIVAL_MAP_BASE64 = [chunk0, chunk1, chunk2, chunk3a, chunk3b, chunk4].join("");
+export const FESTIVAL_MAP_BYTES = 51043;
+export const FESTIVAL_MAP_WIDTH = 640;
+export const FESTIVAL_MAP_HEIGHT = 800;
