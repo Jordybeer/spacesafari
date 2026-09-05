881ef890933f811d9e422e126832d04a69a774f2
jordybeer <info@jordy.beer>
2026-09-05 23:17:14 +0200

    asset: assemble festival map source

---
+++ src/data/map-image/index.ts
@@ -0,0 +1,10 @@
+import chunk0 from "./chunk0";
+import chunk1 from "./chunk1";
+import chunk2 from "./chunk2";
+import chunk3 from "./chunk3";
+import chunk4 from "./chunk4";
+
+export const FESTIVAL_MAP_BASE64 = [chunk0, chunk1, chunk2, chunk3, chunk4].join("");
+export const FESTIVAL_MAP_BYTES = 51043;
+export const FESTIVAL_MAP_WIDTH = 640;
+export const FESTIVAL_MAP_HEIGHT = 800;
